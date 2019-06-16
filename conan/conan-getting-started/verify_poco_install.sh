06cd88aed4b3f116e5cbaa8d2cb2cfb3edcad732
Uilian Ries <uilianries@gmail.com>
2019-06-16 19:27:19 -0300

    Add getting started
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/conan-getting-started/verify_poco_install.sh
+++ conan/conan-getting-started/verify_poco_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[-d build ] && conan search Poco/1.9.0@pocoproject/stable | grep Package_ID && echo "done"
+[ -d /root/poco_md5/build ] && [ -f /root/poco_md5/build/conanbuildinfo.cmake ] && conan search Poco/1.9.0@pocoproject/stable | grep Package_ID && echo "done"
