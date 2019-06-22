55cbd3ff519e2103355860f70f0a3fee75110499
Uilian Ries <uilianries@gmail.com>
2019-06-22 12:20:39 -0300

    Force libstdc++11
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/conan-getting-started/courseBase.sh
+++ conan/conan-getting-started/courseBase.sh
@@ -4,5 +4,7 @@ apt-get update
 apt-get -y install cmake
 pip install conan
 conan config set log.trace_file=/tmp/conan_trace.log
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
 
 echo "done" >> /katacoda-background-finished
