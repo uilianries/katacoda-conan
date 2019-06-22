637196d90c9f4cabe97c1e409afc90c88862ccc3
Uilian Ries <uilianries@gmail.com>
2019-06-22 16:25:18 -0300

    Fix verify scripts
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/conan-using-packages/verify_override_req.sh
+++ conan/conan-using-packages/verify_override_req.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 conan search Poco/1.9.0@pocoproject/stable | grep Package_ID && \
 conan search OpenSSL/1.0.2s@conan/stable | grep Package_ID && \
 conan search zlib/1.2.11@conan/stable | grep Package_ID && \
