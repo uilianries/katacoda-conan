178218155ffe1c9935befca9cd9534ad7bc974e0
Uilian Ries <uilianries@gmail.com>
2019-06-23 19:42:15 -0300

    Remove conan_hello
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/conan-creating-packages/verify_step1.0.sh
+++ conan/conan-creating-packages/verify_step1.0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[ -f /root/conan_hello/conanfile.py ] && [ -f /root/conan_hello/test_package/conanfile.py ] && echo "done"
+[ -f /root/conanfile.py ] && [ -f /root/test_package/conanfile.py ] && echo "done"
