0fccba19e2056d00be226c8dc77f9dc113d6e87b
Uilian Ries <uilianries@gmail.com>
2019-06-23 20:45:27 -0300

    Verify 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/conan-creating-packages/verify_step2.0.sh
+++ conan/conan-creating-packages/verify_step2.0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -v 'def source' conanfile.py && grep 'scm' conanfile.py && conan search Hello/0.1@conan/stable | grep Package_ID && echo "done"
+grep 'scm' conanfile.py && conan search Hello/0.1@conan/stable | grep Package_ID && echo "done"
